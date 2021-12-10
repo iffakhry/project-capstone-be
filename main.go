975bda9dd081d5021efec5b91fa20977c7f0a6eb
Sahril Mahendra <sahrilmahendra@gmail.com>
2021-12-10 07:58:07 +0700

    CI/CD

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	config.InitDB()
 	e := routes.New()
+	// take notes http method activity
 	middlewares.LogMiddlewares(e)
 	// start on port 8080
 	e.Logger.Fatal(e.Start(":8080"))
