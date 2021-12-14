8d954307fccd3c71b9fe3912d615ccf2455ad6ec
Sahril Mahendra <sahrilmahendra@gmail.com>
2021-12-14 22:09:58 +0700

    fix validasi register

--- controllers/users_controllers.go
+++ controllers/users_controllers.go
@@ -9,7 +9,6 @@ import (
 	"log"
 	"net/http"
 	"strconv"
-	"strings"
 
 	validator "github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
@@ -52,7 +51,7 @@ func CreateUserControllers(c echo.Context) error {
 
 	v := validator.New()
 	err := v.Var(new_user.Name, "required,alphanumeric")
-	if err != nil && len(strings.TrimSpace(new_user.Name)) == 0 {
+	if err != nil && len(new_user.Name) > 0 && new_user.Name != "" {
 		return c.JSON(http.StatusBadRequest, response.BadRequestResponse("Invalid Name"))
 	}
 	err = v.Var(new_user.Email, "required,email")
